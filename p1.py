c324b30696cfcb95316494b07f5c6ae9b7583c0a
Sara Chen <sarachen.tw@gmail.com>
2021-10-31 17:41:44 +0800

    calc length of review

--- p1.py
+++ p1.py
@@ -8,4 +8,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: #如果是一千的倍數才印出來
 				print(len(data)) # 計算資料筆數 
 # print(data[0]) #印出清單中的第一筆資料
+print('檔案讀取完了,一共有', len(data),'筆資料')
 
+#算出留言平均長度
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data)) 
